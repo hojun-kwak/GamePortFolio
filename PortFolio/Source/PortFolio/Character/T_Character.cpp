3611c2bb907b886356df24c0abe26aa6ec6bc87a
hojun-kwak <65530716+hojun-kwak@users.noreply.github.com>
2024-09-23 18:13:07 +0900

    playerSetting
    move, jump

---
+++ PortFolio/Source/PortFolio/Character/T_Character.cpp
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PortFolio/Character/T_Character.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+AT_Character::AT_Character()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// 카메라 생성
+	_camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Player Camera"));
+	_camera->SetupAttachment(RootComponent);
+	_camera->bUsePawnControlRotation = true; // 폰제어 회전
+}
+
+// Called when the game starts or when spawned
+void AT_Character::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AT_Character::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AT_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	// IE_Pressed - 눌렀을때
+	// IE_Released - 땟을떄
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AT_Character::Jump);
+	PlayerInputComponent->BindAxis("MoveForward", this, &AT_Character::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AT_Character::MoveRight);
+
+	PlayerInputComponent->BindAxis("TurnCamera", this, &AT_Character::Turn);
+	PlayerInputComponent->BindAxis("LookUp", this, &AT_Character::LookUp);
+}
+
+void AT_Character::MoveForward(float InputValue)
+{
+	FVector ForwardDirection = GetActorForwardVector();
+	AddMovementInput(ForwardDirection, InputValue);
+}
+
+void AT_Character::MoveRight(float InputValue)
+{
+	FVector RightDirection = GetActorRightVector();
+	AddMovementInput(RightDirection, InputValue);
+}
+
+void AT_Character::Turn(float InputValue)
+{
+	AddControllerYawInput(InputValue);
+}
+
+void AT_Character::LookUp(float InputValue)
+{
+	AddControllerPitchInput(InputValue);
+}
+
---
+++ PortFolio/Source/PortFolio/Character/T_Character.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "T_Character.generated.h"
+
+UCLASS()
+class PORTFOLIO_API AT_Character : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AT_Character();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// 프로퍼티가 언리얼 엔진 및 에디터에 이러한 프로퍼티가 있음을 알리고, 프로퍼티가 엔진과 연결되었을 때 어떻게 작동할지를 지정하기 위한 것
+
+	UPROPERTY(EditAnyWhere) // ue의 권한 부여
+	class UCameraComponent* _camera;
+
+	// FVector2D
+	void MoveForward(float InputValue);
+	void MoveRight(float InputValue);
+
+	void Turn(float InputValue);
+	void LookUp(float InputValue);
+};
